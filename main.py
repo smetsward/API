6d26a9b142e01bac78e3db172c9c1713a305983c
smetsward <wardsmets.code@gmail.com>
2022-11-11 11:40:43 +0100

    test11

--- main.py
+++ main.py
@@ -6,3 +6,27 @@ app = FastAPI()
 @app.get("/message")
 async def root():
     return {"message": "hello world"}
+
+@app.get("/")
+def root():
+    return "todooo"
+
+@app.post("/todo")
+def create_todo():
+    return "create todo item"
+
+@app.get("/todo/{id}")
+def read_todo(id: int):
+    return "read todo item with id {id}"
+
+@app.put("/todo/{id}")
+def update_todo(id: int):
+    return "update todo item with id {id}"
+
+@app.delete("/todo/{id}")
+def delete_todo(id: int):
+    return "delete todo item with id {id}"
+
+@app.get("/todo")
+def read_todo_list():
+    return "read todo list"
